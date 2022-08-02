1ee0396064d69dda298f0ec7eee2a50a6527e59c
DESKTOP-DKA2AVR\FRS <alejoruasuarez@gmail.com>
2022-08-02 18:30:20 -0500

    Object mother para PresupuestoValor

---
+++ src/test/java/com/ars/gastos/presupuesto/domain/PresupuestoValorMother.java
@@ -0,0 +1,15 @@
+package com.ars.gastos.presupuesto.domain;
+
+import java.util.Random;
+
+public final class PresupuestoValorMother {
+    public static PresupuestoValor create(double monto) { return new PresupuestoValor(monto); }
+
+    public static PresupuestoValor random() {
+        double min = -100000000000d;
+        double max = 100000000000d;
+
+        Random random = new Random();
+        return create(min + (max - min) * random.nextDouble());
+    }
+}
